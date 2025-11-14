b01ba5caa85f26c1cdf31020b7b10577821977a0
Rishi94523 <rishidv2005@gmail.com>
2025-11-14 08:34:44 +0530

    Update app title from 'Chat with PDFS' to 'AI Search'

--- app/app.py
+++ app/app.py
@@ -26,8 +26,8 @@ class ChatApp:
             os.makedirs("docs")
 
         # Configurations and session state initialization
-        st.set_page_config(page_title="Chat with PDFS :books:")
-        st.title("Chat with PDFS :books:")
+        st.set_page_config(page_title="AI Search")
+        st.title("AI Search")
         initialize_session_state_variables(st)
         self.docs_files = st.session_state.processed_documents
 
